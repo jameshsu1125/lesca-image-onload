c0ffc3c8bcc39c0ef091a62a154872bf1b8f29b5
James Hsu <jameshsu1125@gmail.com>
2025-08-06 11:48:31 +0800

    feat: 重構 ImagePreloader 類別並更新 HTML 結構以支援圖片預載

--- src/root.ts
+++
@@ -1,7 +0,0 @@
-import createApp from ".";
-
-const appElement = document.getElementById("app");
-if (appElement && appElement.children.length === 0) {
-  const app = createApp();
-  appElement.appendChild(app);
-}
